acb282f4ffdb843b8604b9669a58c8a10051d08f
Swapnil-9696 <97024740+Swapnil-9696@users.noreply.github.com>
2022-02-19 19:02:46 +0530

    Add files via upload

---
+++ Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DotNetEndExam.Models
+{
+    public class Products
+    {
+        
+        
+        public int ProductId { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter product name..")]
+        public string ProductName { get; set; }
+
+        
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter product's Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter product's Category name")]
+        public string CategoryName { get; set; }
+    }
+}
